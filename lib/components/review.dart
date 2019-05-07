e17a19ef237e7fecc607eda752a993f9e9f24d9f
Mario Vizcaino <mario.vizcainos@gmail.com>
2019-05-07 10:39:40 -0500

    display review info list

--- lib/components/review.dart
+++ lib/components/review.dart
@@ -1,11 +1,74 @@
 import 'package:flutter/material.dart';
+import 'stars_widget.dart';
 
 class Review extends StatelessWidget {
   String pathImage = 'lib/assets/img/people.jpg';
+  String name = 'Varuna Yasas';
+  String detail = '1 Review 5 photos';
+  String comment = 'There is a amazin place in Sri Lanka';
+  double stars;
 
-  Review(this.pathImage);
+  Review(this.pathImage, this.name, this.detail, this.comment, this.stars);
+
   @override
   Widget build(BuildContext context) {
+    final userComment = Container(
+      margin: EdgeInsets.only(
+        left: 20
+      ),
+      child: Text(
+        comment,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontFamily: 'Lato',
+          fontSize: 13,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+    );
+
+    final userInfo = Container(
+      margin: EdgeInsets.only(
+        left: 20
+      ),
+      child: Row(
+        children: <Widget>[
+          Text(
+            detail,
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontFamily: 'Lato',
+              fontSize: 13
+            ),
+          ),
+          starsWidget(stars, top: 0, size: 15),
+        ],
+      ),
+    );
+
+    final userName = Container(
+      margin: EdgeInsets.only(
+        left: 20
+      ),
+      child: Text(
+        name,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontFamily: 'Lato',
+          fontSize: 17,
+          color: Color(0xFFa3a5a7),
+        ),
+      ),
+    );
+
+    final userDetails = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        userName,
+        userInfo,
+        userComment,
+      ],
+    );
     final photo = Container(
       margin: EdgeInsets.only(
         top: 20,
@@ -24,7 +87,8 @@ class Review extends StatelessWidget {
 
     return Row(
       children: <Widget>[
-
+        photo,
+        userDetails,
       ],
     );
   }
---
+++ lib/components/review_list.dart
@@ -0,0 +1,56 @@
+
+import 'package:flutter/material.dart';
+import 'review.dart';
+
+class ReviewList extends StatelessWidget {
+  List<Map<String, dynamic>> data = [
+    {
+      'image': 'lib/assets/img/people.jpg',
+      'name': 'Varuna yasas',
+      'detail': '1 Review 5 photos',
+      'comment': 'There is a amazin place in Sri Lanka',
+      'stars': 5.0,
+    },
+    {
+      'image': 'lib/assets/img/people.jpg',
+      'name': 'Varuna yasas',
+      'detail': '1 Review 5 photos',
+      'comment': 'There is a amazin place in Sri Lanka',
+      'stars': 4.0,
+    },
+    {
+      'image': 'lib/assets/img/people.jpg',
+      'name': 'Varuna yasas',
+      'detail': '1 Review 5 photos',
+      'comment': 'There is a amazin place in Sri Lanka',
+      'stars': 1.0,
+    },
+    {
+      'image': 'lib/assets/img/people.jpg',
+      'name': 'Varuna yasas',
+      'detail': '1 Review 5 photos',
+      'comment': 'There is a amazin place in Sri Lanka',
+      'stars': 3.5,
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: renderReview(),
+    );
+  }
+  List<Widget> renderReview(){
+    List<Widget> list = [];
+    data.forEach((el) => {
+      list.add(
+        Review(
+          el['image'],
+          el['name'],
+          el['detail'],
+          el['comment'],
+          el['stars'])
+      )
+    });
+    return list;
+  }
+}
