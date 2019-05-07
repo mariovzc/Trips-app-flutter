6d71f653cadc59a6046875573b5ae52a1d2656d6
Mario Vizcaino <mario.vizcainos@gmail.com>
2019-05-07 10:39:21 -0500

    refactor of stars widgets

--- lib/components/star_widget.dart
+++ lib/components/star_widget.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-Widget starsRender(IconData icon, Color color) {
+Widget starsRender(IconData icon, Color color, double top,double size) {
   return Container(
     margin: EdgeInsets.only(
-      top: 323,
+      top: top,
       right: 3
     ),
     child: Icon(
       icon,
       color: color,
+      size: size,
     ),
   );
 }
--- lib/components/stars_widget.dart
+++ lib/components/stars_widget.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'star_widget.dart';
 
-Widget starsWidget(double numberOfStars){
+Widget starsWidget(double numberOfStars, {double top = 323,double size = 25}){
   List<Widget> rowStars = [];
   for(int i=1; i<=5; i++){
-    rowStars.add(validateStarIconWidget(numberOfStars));
+    rowStars.add(validateStarIconWidget(numberOfStars, top, size));
     numberOfStars--;
   }
 
@@ -13,12 +13,12 @@ Widget starsWidget(double numberOfStars){
   );
 }
 
-Widget validateStarIconWidget(double numberOfStars){
+Widget validateStarIconWidget(double numberOfStars, double top, double size){
   if(numberOfStars >= 1.0){
-    return starsRender(Icons.star, Color(0XFFF2C611));
+    return starsRender(Icons.star, Color(0XFFF2C611), top, size);
   } else if (numberOfStars >= 0.5) {
-    return starsRender(Icons.star_half, Color(0XFFF2C611));
+    return starsRender(Icons.star_half, Color(0XFFF2C611), top, size);
   } else {
-    return starsRender(Icons.star_border, Color(0XFFF2C611));
+    return starsRender(Icons.star_border, Color(0XFFF2C611), top, size);
   }
 }
