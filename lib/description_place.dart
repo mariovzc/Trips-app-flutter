0d272cb28c51309b1a93f69fbd298d304f7373b3
Mario Vizcaino <mario.vizcainos@gmail.com>
2019-06-06 22:10:51 -0500

    setup full project

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,39 +1,120 @@
 import 'package:flutter/material.dart';
-import 'package:trips_app/components/home_widgets/button_purple.dart';
-import 'package:trips_app/components/home_widgets/description_text.dart';
-import 'package:trips_app/components/home_widgets/title_place.dart';
-import 'components/stars_widget.dart';
+import 'button_purple.dart';
 
 class DescriptionPlace extends StatelessWidget {
-    String namePlace;
-    double stars;
-    String descriptionPlace;
+
+  String namePlace;
+  int stars;
+  String descriptionPlace;
 
   DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
 
+
+
   @override
   Widget build(BuildContext context) {
-    final header = Row(
+    // TODO: implement build
+
+    final star_half = Container (
+      margin: EdgeInsets.only(
+          top: 353.0,
+          right: 3.0
+      ),
+
+      child: Icon(
+        Icons.star_half,
+        color:  Color(0xFFf2C611),
+      ),
+    );
+
+    final star_border = Container (
+      margin: EdgeInsets.only(
+          top: 353.0,
+          right: 3.0
+      ),
+
+      child: Icon(
+        Icons.star_border,
+        color:  Color(0xFFf2C611),
+      ),
+    );
+
+    final star = Container (
+      margin: EdgeInsets.only(
+        top: 353.0,
+        right: 3.0
+      ),
+
+      child: Icon(
+        Icons.star,
+        color:  Color(0xFFf2C611),
+      ),
+    );
+
+    final title_stars = Row (
       children: <Widget>[
-        titlePlace(namePlace),
-        starsWidget(stars)
+        Container (
+          margin: EdgeInsets.only(
+            top: 350.0,
+            left: 20.0,
+            right: 20.0
+          ),
+
+          child: Text(
+            namePlace,
+            style: TextStyle(
+              fontFamily: "Lato",
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900
+            ),
+            textAlign: TextAlign.left,
+          ),
+
+        ),
+
+        Row(
+          children: <Widget>[
+            star,
+            star,
+            star,
+            star,
+            star_half
+          ],
+        )
+
+
       ],
+    );
+
+    final description = Container(
+      margin: new EdgeInsets.only(
+          top: 20.0,
+          left: 20.0,
+          right: 20.0
+
+      ),
+      child: new Text(
+        descriptionPlace,
+        style: const TextStyle(
+            fontFamily: "Lato",
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF56575a)
+        ),
+
+      ),
     );
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        header,
-        descriptionText(descriptionPlace),
-        ButtonPurple('Navigate', _sendAlert),
+        title_stars,
+        description,
+        ButtonPurple("Navigate")
       ],
     );
+
+
   }
-  _sendAlert(BuildContext context) {
-    Scaffold.of(context).showSnackBar(
-      SnackBar(
-        content: Text('on Tap'),
-      )
-    );
-  }
+
 }
